0f362de6eefd59dfd52e0e08db84cdeaf640fe64
Wen Xiao <wennstyle@gmail.com>
2022-03-06 22:12:55 -0500

    material ui dummy table

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import TableCell from '@material-ui/core/TableCell';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
 
 const TaskList = ({tasks}) => {
-    console.log(tasks)
-  return (
-    <div>
-      <h3>Display Tasks!</h3>
 
-    </div>
+  const {taskName, tickets, status} = tasks
+  return (
+    // <div>
+    //   <h3>{taskName}</h3>
+    // </div>
+    <TableBody>
+    <TableRow>
+            <TableCell component="th" scope="row">
+            dummydata
+            </TableCell>
+            <TableCell align="right">dummydata</TableCell>
+            <TableCell align="right">dummydata</TableCell>
+            <TableCell align="right">dummydata</TableCell>
+            <TableCell align="right">dummydata</TableCell>
+            </TableRow>
+        </TableBody>
+
   )
 }
 
--- src/Components/TaskPage.js
+++ src/Components/TaskPage.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import TaskForm from './TaskForm.js';
 import TaskList from './TaskList.js';
+import Table from '@material-ui/core/Table';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
 
 function TaskPage({allTasks}) {
   const displayTasks = allTasks.map((task) => (
@@ -9,12 +17,39 @@ function TaskPage({allTasks}) {
     tasks = {task}
     />
   ))
-  
+
+  //for styling
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+  const classes=useStyles()
+  //for styling
 
   return (
+    
     <div>
     <TaskForm />
-    {displayTasks}
+    <h1 align="center">Display Tasks!</h1>
+    <TableContainer component={Paper}>
+    <Table className={classes.table} aria-label="simple table">
+    <TableHead>
+        <TableRow>
+          <TableCell>Tasks</TableCell>
+          <TableCell align="right">Frequency</TableCell>
+          <TableCell align="right">Date</TableCell>
+          <TableCell align="right">Reward</TableCell>
+          <TableCell align="right">Status</TableCell>
+        </TableRow>
+    </TableHead>
+        {displayTasks}
+
+
+    </Table>
+  </TableContainer>
+
     </div>
   )
 }
